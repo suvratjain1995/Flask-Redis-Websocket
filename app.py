9d32583e20fd16847a77ebfaad7859217c5d71c5
Suvrat <etherjain@gmail.com>
2019-01-03 03:18:00 +0530

    Stats Completed

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import datetime
 import time
 import random
 
-@app.before_first_request
-def set_context():
-    g.server_start = time.time()
-    g.response_list = []
+class Records:
+    records = []
+    def __init__(self):
+        pass
 
+r = Records()
 @app.before_request
 def start_time():
     now = time.time()
@@ -20,8 +21,11 @@ def average_request(reponse_list,method_name):
     average_request_result = 0
     for responses in reponse_list:
         if responses["Method"] == method_name:
-            average_request_result+=1
-    return average_request_result/len(reponse_list)
+            average_request_result+=responses["duration"]
+    if len(reponse_list)>0:
+        return average_request_result/len(reponse_list)
+    else:
+        return 0;
 
 def number_of_method_request(response_list,method_name):
     number_of_request = 0;
@@ -31,12 +35,12 @@ def number_of_method_request(response_list,method_name):
     return number_of_request;
 
 def save_incoming_request(response):
-    g.response_list.append(response)
+    Records.records.append(response)
 
 def minute_stat(method_list):
     valid_responses = []
     current_time = g.start
-    for responses in g.response_list:
+    for responses in Records.records:
         temp_recieve_time = responses["Time"]
         if temp_recieve_time.day == current_time.day and \
             temp_recieve_time.hour == current_time.hour and \
@@ -56,7 +60,7 @@ def minute_stat(method_list):
 def hour_stat(method_list):
     valid_responses = []
     current_time = g.start
-    for responses in g.response_list:
+    for responses in Records.records:
         temp_recieve_time = responses["Time"]
         if temp_recieve_time.day == current_time.day and \
             temp_recieve_time.hour == current_time.hour and \
@@ -77,8 +81,8 @@ def general_stat(method_list):
     for methods in method_list:
         method_avg = methods+"_hour_avg"
         method_count = methods+"_hour_count"
-        general_stat_dict[method_avg] = average_request(g.reponse_list,methods)
-        general_stat_dict[method_count] = number_of_method_request(g.response_list,methods)
+        general_stat_dict[method_avg] = average_request(Records.records,methods)
+        general_stat_dict[method_count] = number_of_method_request(Records.records,methods)
     return general_stat_dict
 
 @app.route('/stats',methods = ['GET', 'POST', 'PUT', 'DELETE'])
@@ -93,7 +97,7 @@ def get_stat():
 @app.route('/process/',methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def entrypoint():
     response = {}
-    time1 = random.randint(15,30)
+    time1 = random.randint(2,3)
     time.sleep(time1)
     response["Time"] = g.start
     response["Method"] = request.method
@@ -101,7 +105,7 @@ def entrypoint():
     response["Query"]= request.args
     response["Body"]= request.json
     response["duration"]= time1
-
+    save_incoming_request(response)
     return jsonify(response)
 
 if __name__=='__main__':
